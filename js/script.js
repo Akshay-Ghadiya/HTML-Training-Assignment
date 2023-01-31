398fe87957a3aa90cfadaa8e29cbb6fa5b1211a2
Akshay-Ghadiya <akshayghadiya7@gmail.com>
2023-01-31 15:59:26 +0530

    commit 4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
-function popup(){
+function SideBar(){
 
-    document.getElementById("sidebarr").style.display = none;
+    
+    var x = document.getElementById("sidebarr");
 
+    if(x.style.display == "block"){
+        document.getElementById("sidebarr").style.display = "none";
+    }
+    else{
+        document.getElementById("sidebarr").style.display = "block";
+    }
+}
+
+
+function FirstTab(){
+    document.getElementById("tab1-content").style.display = "block";
+    document.getElementById("tab2-content").style.display = "none";
+    document.getElementById("tab3-content").style.display = "none";
+    document.getElementById("button1").style.color = "red";
+    document.getElementById("button2").style.color = "black";
+    document.getElementById("button3").style.color = "black";
+ 
+
+}
+function SecondtTab(){
+    document.getElementById("tab1-content").style.display = "none";
+    document.getElementById("tab2-content").style.display = "block";
+    document.getElementById("tab3-content").style.display = "none";
+    document.getElementById("button2").style.color = "red";
+    document.getElementById("button1").style.color = "black";
+    document.getElementById("button3").style.color = "black";
+}
+function ThirdTab(){
+    document.getElementById("tab1-content").style.display = "none";
+    document.getElementById("tab2-content").style.display = "none";
+    document.getElementById("tab3-content").style.display = "block";
+    document.getElementById("button3").style.color = "red";
+    document.getElementById("button1").style.color = "black";
+    document.getElementById("button2").style.color = "black";
 }
